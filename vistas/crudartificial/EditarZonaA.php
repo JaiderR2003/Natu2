b9e49429c5cf7f2239b39c0f3de6857f9d3192da
curregof <dcurrego35@misena.edu.co>
2021-11-10 11:52:02 -0500

    Pagina version 2.0

--- vistas/crudartificial/EditarZonaA.php
+++ vistas/crudartificial/EditarZonaA.php
@@ -11,6 +11,7 @@ include '../../modelo/ControladorArtificial/DatosARController.php';
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="Imagenes/LogoNa.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap CSS v5.0.2 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -68,7 +69,8 @@ include '../../modelo/ControladorArtificial/DatosARController.php';
         <legend class="containter-xl"> Contactenos </legend><br>
         <p class="containter-xl"> Linea Gratuita: <b>01 8000 8888</b> </p>
         <p class="containter-xl"> Correo: <b>NatuPoints@gmail.com </b></p>
-        <br>
+        <li style="display:inline-block;"><a style="color:white;font-size:45px;padding:10px 5px;display:block;transition:all 0.5 ease-in-out;" href="https://www.facebook.com/NatuPoints/" target="blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+        <li style="display:inline-block;"><a style="color:white;font-size:45px;padding:10px 5px;display:block;transition:all 0.5 ease-in-out;" href="https://www.instagram.com/natupoints/" target="blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
     </footer>
 </body>
 </html>
--- vistas/paginas/template.php
+++ vistas/paginas/template.php
@@ -6,6 +6,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="shortcut icon" href="Imagenes/LogoNa.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- Bootstrap CSS v5.0.2 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
@@ -43,12 +44,13 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
  
-    <footer class="containter-xl" style=" border: solid #9de0ad;border: 8px;border-radius: 12px;background-color: #9de0ad;text-align: center;margin-left: 85px;margin-right: 75px;margin-top: -70px;margin-bottom: 50px;color: #41444b;font-family: 'Ubuntu', sans-serif;">
+    <footer class="containter-xl" style=" border: solid #9de0ad;border: 8px;border-radius: 12px;background-color: #9de0ad;text-align: center;margin-left: 85px;margin-right: 75px;margin-top: -70px;margin-bottom: 50px;color: #41444b;font-family: 'Ubuntu', sans-serif;list-style:none;align-content:center;">
         <br>
         <legend class="containter-xl"> Contactenos </legend><br>
         <p class="containter-xl"> Linea Gratuita: <b>01 8000 8888</b> </p>
         <p class="containter-xl"> Correo: <b>NatuPoints@gmail.com </b></p>
-        <br>
+        <li style="display:inline-block;"><a style="color:white;font-size:45px;padding:10px 5px;display:block;transition:all 0.5 ease-in-out;" href="https://www.facebook.com/NatuPoints/" target="blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+        <li style="display:inline-block;"><a style="color:white;font-size:45px;padding:10px 5px;display:block;transition:all 0.5 ease-in-out;" href="https://www.instagram.com/natupoints/" target="blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
     </footer>
 </body>
 </html>
